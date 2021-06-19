476b7a94c707e55c70661dca14d701240f0b954a
TonyRandom <65846634+TonyRandom@users.noreply.github.com>
2021-06-19 16:40:55 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// инициализация плагина. как с библиотекой сторонней.
+import {Select} from './select/select';
+import './select/styles.scss';
+
+const select = new Select('#select', { // передаем элемент, который должен стать селектом и параметры в объекте
+    placeholder: 'Выбери, пожалуйста, элемент',
+    selectedId: '4',
+    data: [
+        {id: '1', value: 'React'},
+        {id: '2', value: 'Angular'},
+        {id: '3', value: 'Vue'},
+        {id: '4', value: 'React Native'},
+        {id: '5', value: 'Next'},
+        {id: '6', value: 'Nest'}
+    ],
+    onSelect (item) {
+        console.log('Selected item', item);
+    }
+});
+
+window.s = select;
